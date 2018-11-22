d6c68935b621102dc0c159a7fef15add9306b284
Mazen Touati <mazen_touati@hotmail.com>
2018-11-22 16:48:38 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## mazentouati/snacksconsole
+
+This is the website's repository for snacks console.
+
+# What's snacks console ?
+
+Snacks console is an intuitive browser-based SQL console to run and save SQL queries.
+
+## LICENSE
+
+> &copy; [MIT](./LICENSE) | 2018, mazentouati/snacks-console
---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+global.destDir = './dist';
+global.srcDir = './src';
+
+const 
+  
+  gulp        = require('gulp'),
+  browserSync = require('browser-sync').create()
+
+  buildSASS   = require('./tasks/build/sass'),
+  buildJS     = require('./tasks/build/js'),
+  buildHTML   = require('./tasks/build/html'),
+  buildStatic = require('./tasks/build/static')
+
+  exportJS      = require('./tasks/export/js'),
+  exportHTML    = require('./tasks/export/html'),
+  exportStatic  = require('./tasks/export/static'),
+  exportCSS     = require('./tasks/export/css')
+;
+
+gulp.task('export', function() {
+  exportJS();
+  exportHTML();
+  exportCSS();
+  exportStatic();
+});
+
+gulp.task('export:js', exportJS);
+gulp.task('export:html', exportHTML);
+gulp.task('export:static', exportStatic);
+gulp.task('export:css', exportCSS);
+
+
+gulp.task('build:sass', buildSASS);
+gulp.task('build:js', buildJS);
+gulp.task('build:html', buildHTML);
+gulp.task('build:static', buildStatic);
+
+gulp.task('build', function() {
+  buildSASS();
+  buildJS();
+  buildHTML();
+  buildStatic();
+});
+
+gulp.task('serve', ['build'], function () {
+  browserSync.init({
+    server: {
+      baseDir: ['./dist/'],
+    },
+    notify: false,
+    ui: false,
+    open: false,
+    injectChanges: true,
+  });
+
+  gulp.watch('./src/scss/**/*', ['reload:sass']);
+  gulp.watch('./src/js/**/*', ['reload:js']);
+  gulp.watch('./src/html/**/*', ['reload:html']);
+
+});
+
+gulp.task('reload:sass', function () {
+  buildSASS().on('end', function() {
+    browserSync.reload();
+  });
+});
+
+gulp.task('reload:js', function () {
+  buildJS().on('end', function() {
+    browserSync.reload();
+  });
+});
+
+gulp.task('reload:html', function () {
+  buildHTML().on('end', function() {
+    browserSync.reload();
+  });
+});
