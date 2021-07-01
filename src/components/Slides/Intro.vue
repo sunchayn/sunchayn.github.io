7221f0e3c44f11905d80c86657a2daa0469269df
Mazen Touati <mazen_touati@hotmail.com>
2021-07-01 18:40:07 +0100

    Update resume

--- src/components/Slides/Intro.vue
+++ src/components/Slides/Intro.vue
@@ -13,7 +13,7 @@
         An honest, innovative and minimalistic individual who enjoys talking to both computers and humans.
       </p>
       <a
-        href="https://sunchayn.github.io/Resume%20-%20Mazen%20Touati%20-%20Jun%202021.pdf"
+        href="https://sunchayn.github.io/Resume%20-%20Mazen%20Touati%20-%20June%202021.pdf"
         class="button sr-hook"
         rel="noopener"
       ><small class="text-xs lowercase"><i>pdf&nbsp;&mdash;&nbsp;</i></small>Resumé</a>
